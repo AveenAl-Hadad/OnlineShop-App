5fd24aec890cfe998eef3e99f48dcc58fbadfee4
Aveen alhadad <aveen_alhadad@yahoo.com>
2025-05-19 13:46:42 +0200

    Bild-Galerie unter der Navigation einfügen

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -182,6 +182,46 @@ $(document).ready(function () {
   // Lade externe HTML-Blöcke
   $('#add-product-container').load('add-product.html');
   $('#edit-product-container').load('edit-product.html');
-  $('#slideshow').load('galary-bilder.html');
+  
   $('#cartModal').load('warenkorp.html');
+});
+//jQuery – Automatischer Bildwechsel für Galary bilder startseite
+$(document).ready(function () {
+  let currentSlide = 0;
+  const slides = $('.slide');
+  const totalSlides = slides.length;
+
+  function showSlide(index) {
+    slides.fadeOut(500);
+    slides.eq(index).fadeIn(500);
+  }
+
+  function showNextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+  }
+
+  function showPrevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+  }
+
+  // Automatisch wechseln
+  let autoSlide = setInterval(showNextSlide, 3000);
+
+  // Manuelle Steuerung
+  $('.next').on('click', function () {
+    clearInterval(autoSlide);
+    showNextSlide();
+    autoSlide = setInterval(showNextSlide, 3000);
+  });
+
+  $('.prev').on('click', function () {
+    clearInterval(autoSlide);
+    showPrevSlide();
+    autoSlide = setInterval(showNextSlide, 3000);
+  });
+
+  // Erste Slide anzeigen
+  showSlide(currentSlide);
 });
